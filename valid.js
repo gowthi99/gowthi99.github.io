e5f2a6b47f30cee0036531394f3e7022a067fe24
gowthi99 <gowtham1999.kkr@gmail.com>
2023-03-31 14:41:52 +0530

    Px-commit1

--- valid.js
+++ valid.js
@@ -1,3 +1,10 @@
+
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VA95L0WUKTBX-2");
+
 function customfunc(){
 
 var x =document.getElementById("pass");
@@ -15,7 +22,7 @@ function validate(){
 var p = document.getElementById("pass");
 var lenght = document.getElementById("length");
 
-
+         
 if(p.value.length <8){
     alert("Password must be atleast 8 characters long");
     false;
